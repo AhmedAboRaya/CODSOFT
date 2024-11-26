e60407b1e07840906a9de14c495f0a4dc47c4d7d
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2024-11-26 06:05:42 +0200

    k

--- ShopSphere/client/src/components/signUp/SignUp.jsx
+++ ShopSphere/client/src/components/signUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { host } from '../../host.js'; 
+import { host } from "../../host.js";; 
 import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
